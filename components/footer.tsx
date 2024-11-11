ac8e4c259104fffb992612512f02bb9cc0fcd1fc
Prathamesh Ratthe <97759958+prathameshratthe@users.noreply.github.com>
2024-11-11 11:37:22 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,26 +6,6 @@ export const Footer = () => {
     return (
         <div className="container my-10 flex flex-col items-center justify-start gap-5 md:flex-row">
             <ThemeToggle />
-            <p className="text-left font-mono text-xs text-muted-foreground">
-                made with ❤️ by{" "}
-                <Link
-                    className="underline"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://github.com/metaloozee/"
-                >
-                    metaloozee
-                </Link>{" "}
-                •{" "}
-                <Link
-                    className="underline"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://www.buymeacoffee.com/metaloozee"
-                >
-                    support
-                </Link>
-            </p>
         </div>
     )
 }
